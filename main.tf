3fa374ece3a0d2d41f1869d3092f7c7792f4a17a
devopsrealteam3 <devopsrealteam3@gmail.com>
2022-11-22 01:17:34 +0530

    new changes

--- main.tf
+++ main.tf
@@ -1,12 +1,21 @@
+# The following configuration uses a provider which provisions [fake] resources
+# to a fictitious cloud vendor called "Fake Web Services". Configuration for
+# the fakewebservices provider can be found in provider.tf.
+#
+# After running the setup script (./scripts/setup.sh), feel free to change these
+# resources and 'terraform apply' as much as you'd like! These resources are
+# purely for demonstration and created in Terraform Cloud, scoped to your TFC
+# user account.
+#
+# To review the provider and documentation for the available resources and
+# schemas, see: https://registry.terraform.io/providers/hashicorp/fakewebservices
+#
+# If you're looking for the configuration for the remote backend, you can find that
+# in backend.tf.
 
-provider "aws" {
-  region = "ap-south-1"
-  access_key = "var.my_access_key"
-  secret_key = "var.my_secret_key"
-}
 
 resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+  name        = "allow_tls_22nov222211"
   description = "Allow TLS inbound traffic"
   vpc_id      = "vpc-0e0f84311b585d9b0"
 
@@ -30,4 +39,3 @@ resource "aws_security_group" "allow_tls" {
     Name = "allow_tls"
   }
 }
-
--- variables.tf
+++
@@ -1,7 +0,0 @@
-variable "my_access_key" {
-    type  = string
-}
-
-variable "my_secret_key" {
-    type  = string
-}
